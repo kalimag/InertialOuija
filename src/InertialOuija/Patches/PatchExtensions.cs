5de090073a29c76689a3d56da14e577764f760db
kalimag <kalimag@users.noreply.github.com>
2025-04-10 17:55:07 +0200

    Fix bizarre Mono crash

--- src/InertialOuija/Patches/PatchExtensions.cs
+++ src/InertialOuija/Patches/PatchExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
@@ -14,8 +15,10 @@ internal static class PatchExtensions
 			var patchedMethods = harmony.CreateClassProcessor(patchClass).Patch();
 
 #if DEBUG
+			// Calling ToString() on one specific MethodInfo suddenly hard crashes Mono, even on older revisions?
+			// No clue why, so let's just not do that and hope for the best I guess
 			if (patchedMethods != null)
-				Log.Info($"Applied {patchClass.Name}:\n" + String.Join("\n", patchedMethods));
+				Log.Info($"Applied {patchClass.Name}:\n" + String.Join("\n", patchedMethods.Select(m => m.Name)));
 			else
 				Log.Info($"No patches found in {patchClass.Name}");
 #else
