4730c14ac4800511a4921ae909be6eb136e1e63b
kalimag <kalimag@users.noreply.github.com>
2025-04-17 19:35:03 +0200

    Add ISerializable source generator

---
+++ src/InertialOuija.SourceGenerators/CodeWriter.cs
@@ -0,0 +1,87 @@
+﻿using System.CodeDom.Compiler;
+using System.Text;
+using Microsoft.CodeAnalysis.Text;
+
+namespace InertialOuija.SourceGenerators;
+internal class CodeWriter : IndentedTextWriter
+{
+	private readonly MemoryStream _memory;
+
+	public int Indentation => base.Indent;
+
+	private CodeWriter(MemoryStream stream, string tabString) : base(new StreamWriter(stream, Encoding.UTF8), tabString)
+	{
+		_memory = stream;
+	}
+
+	public CodeWriter(string tabString = "\t") : this(new MemoryStream(), tabString)
+	{ }
+
+	public new CodeWriter WriteLine(string line)
+	{
+		base.WriteLine(line);
+		return this;
+	}
+
+	public CodeWriter WriteLine(string line, bool condition)
+	{
+		if (condition)
+			WriteLine(line);
+		return this;
+	}
+
+	public new CodeWriter Write(string line)
+	{
+		base.Write(line);
+		return this;
+	}
+
+	public CodeWriter Write(string line, bool condition)
+	{
+		if (condition)
+			Write(line);
+		return this;
+	}
+
+	public new CodeWriter Indent()
+	{
+		base.Indent++;
+		return this;
+	}
+
+	public CodeWriter Unindent()
+	{
+		base.Indent--;
+		return this;
+	}
+
+	public CodeWriter OpenBrace()
+	{
+		base.WriteLine('{');
+		return Indent();
+	}
+
+	public CodeWriter CloseBrace()
+	{
+		Unindent();
+		base.WriteLine('}');
+		return this;
+	}
+
+
+	public SourceText ToSourceText()
+	{
+		Flush();
+		_memory.Position = 0;
+		var sourceText = SourceText.From(_memory, Encoding.UTF8, canBeEmbedded: true);
+		_memory.Seek(0, SeekOrigin.End);
+		return sourceText;
+	}
+
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing)
+			_memory.Dispose();
+	}
+}
---
+++ src/InertialOuija.SourceGenerators/ValueEquatableArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace InertialOuija.SourceGenerators;
+internal readonly struct ValueEquatableArray<T>(ImmutableArray<T> values) : IReadOnlyList<T>, IEquatable<ValueEquatableArray<T>>
+{
+	public readonly ImmutableArray<T> Values = values;
+
+	public int Count => Values.Length;
+
+	public T this[int index] => Values[index];
+
+	public bool Equals(ValueEquatableArray<T> other)
+	{
+		if (Count != other.Count)
+			return false;
+
+		for (int i = 0; i < Count; i++)
+			if (!EqualityComparer<T>.Default.Equals(this[i], other[i]))
+				return false;
+
+		return true;
+	}
+
+	public override bool Equals(object obj) => obj is ValueEquatableArray<T> array && Equals(array);
+
+	public override int GetHashCode() => -1521134295 * Count;
+
+	public static implicit operator ValueEquatableArray<T>(ImmutableArray<T> values) => new(values);
+	public static implicit operator ImmutableArray<T>(ValueEquatableArray<T> array) => array.Values;
+
+	public ImmutableArray<T>.Enumerator GetEnumerator() => Values.GetEnumerator();
+	IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)Values).GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Values).GetEnumerator();
+}
