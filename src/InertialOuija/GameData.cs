c12122cdb4be4baac93f780e1c6cff9555825b3b
kalimag <kalimag@users.noreply.github.com>
2025-04-18 04:26:24 +0200

    Add `GameData`

---
+++ src/InertialOuija/GameData.cs
@@ -0,0 +1,26 @@
+﻿extern alias GameScripts;
+
+using GameScripts.Assets.Source.Tools;
+using HarmonyLib;
+using System;
+using System.Threading;
+
+namespace InertialOuija;
+
+public static class GameData
+{
+	private static AccessTools.FieldRef<bool> _corePluginInitialized = AccessTools.StaticFieldRefAccess<bool>(AccessTools.Field(typeof(CorePlugin), "_initialised"));
+	public static bool CorePluginInitialized => CorePlugin.CorePluginInstance() && _corePluginInitialized();
+
+
+	private static readonly Lazy<(string, ulong)> _steamUser = new(() =>
+	{
+		if (!GameScripts.SteamManager.Initialized)
+			throw new InvalidOperationException("SteamManager not initialized yet.");
+		return (Steamworks.SteamFriends.GetPersonaName(), Steamworks.SteamUser.GetSteamID().m_SteamID);
+	}, LazyThreadSafetyMode.PublicationOnly);
+
+	public static (string Name, ulong Id) SteamUser => _steamUser.Value;
+	public static (string Name, ulong Id)? TryGetSteamUser() => GameScripts.SteamManager.Initialized ? SteamUser : null;
+
+}
