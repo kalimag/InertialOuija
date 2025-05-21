82451dd540e3620e7de0bff5ad760f6d27f26e1e
kalimag <kalimag@users.noreply.github.com>
2025-05-21 23:51:23 +0200

    Overhaul options window

--- src/InertialOuija/UI/GhostOptionsWindow.cs
+++ src/InertialOuija/UI/GhostOptionsWindow.cs
@@ -13,8 +13,8 @@ namespace InertialOuija.UI
 		private readonly StringCache<int> _countString = new("Ghosts: 0");
 
 
-		protected override Rect InitialPosition => new(710, 100, 200, 50);
 		protected override string Title => "OPTIONS";
+		protected override Rect InitialPosition => new(710, 100, 400, 50);
 		protected override Rect WindowPosition { get => _windowPosition; set => _windowPosition = value; }
 
 		private static Rect _windowPosition;
@@ -28,45 +28,51 @@ namespace InertialOuija.UI
 
 		protected override void DrawWindow()
 		{
-			Config.Ghosts.GhostVisual = !GUILayout.Toggle(!Config.Ghosts.GhostVisual, "Show as regular cars");
 
+			GUILayout.Label("Ghosts", Styles.Heading);
+			Config.Ghosts.GhostVisual = !GUILayout.Toggle(!Config.Ghosts.GhostVisual, "Show as regular cars");
 			Config.Ghosts.DisableHeadlights = GUILayout.Toggle(Config.Ghosts.DisableHeadlights, "Disable headlight effect");
 
-			Styles.Space();
-
-			Config.UI.ShowGhostTime = GUILayout.Toggle(Config.UI.ShowGhostTime, "Show ghost time in hud");
-			Config.UI.ShowPersonalBestTime = GUILayout.Toggle(Config.UI.ShowPersonalBestTime, TempContent("Show personal best in hud", "Based on saved ghosts"));
-			Config.UI.HideAchievedTargetTimes = GUILayout.Toggle(Config.UI.HideAchievedTargetTimes, "Hide medals time in hud");
+			GUILayout.Label("UI", Styles.Heading);
+			Config.UI.ShowGhostTime = GUILayout.Toggle(Config.UI.ShowGhostTime, "Show ghost time in HUD");
+			Config.UI.ShowPersonalBestTime = GUILayout.Toggle(Config.UI.ShowPersonalBestTime, TempContent("Show personal best in HUD", "Based on saved ghosts"));
+			Config.UI.HideAchievedTargetTimes = GUILayout.Toggle(Config.UI.HideAchievedTargetTimes, TempContent("Hide medal target times in HUD", "Hides the gold/silver/bronze display if your PB is better"));
 			Config.UI.ShowChosenGhosts = GUILayout.Toggle(Config.UI.ShowChosenGhosts, "Show ghost list before race");
 
-			Styles.Space();
+			GUILayout.Label("Ghost Files", Styles.Heading);
+			GUILayout.Label(_countString.GetString(ExternalGhostManager.Count));
 
-			if (GUILayout.Button("Open Ghost Folder"))
-				OpenGhostFolder();
-
-			using (Styles.Enable(!ExternalGhostManager.RefreshInProgress))
+			using (Styles.Horizontal())
 			{
-				if (GUILayout.Button(TempContent("Refresh External Ghosts", "Use if you added or removed files while the game is running")))
-					ExternalGhostManager.RefreshDatabaseAsync().LogFailure();
+				using (Styles.Enable(!ExternalGhostManager.RefreshInProgress))
+					if (GUILayout.Button(TempContent("Refresh Ghosts", "Use if you added or removed files while the game is running"), CustomStyles.GhostButtonLayout))
+						ExternalGhostManager.RefreshDatabaseAsync().LogFailure();
 
-				if (GUILayout.Button(TempContent("Reset Ghost Cache", "This shouldn't be necessary unless something goes wrong")))
-					ExternalGhostManager.ResetCache().LogFailure();
-
-				Styles.Space();
-
-				using (Styles.Enable(GameData.CorePluginInitialized))
-					if (GUILayout.Button(TempContent("Export Old Ghosts", "This only needs to be done once")))
-						ExternalGhostManager.ExportPlayerDatabase();
+				if (GUILayout.Button("Open Ghost Folder", CustomStyles.GhostButtonLayout))
+					OpenGhostFolder();
 			}
 
 			Styles.Space();
 
-			GUILayout.Label(_countString.GetString(ExternalGhostManager.Count));
+			using (Styles.Enable(!ExternalGhostManager.RefreshInProgress))
+			using (Styles.Horizontal())
+			{
+				using (Styles.Enable(GameData.CorePluginInitialized))
+					if (GUILayout.Button(TempContent("Export Old Ghosts", "This only needs to be done once"), CustomStyles.GhostButtonLayout))
+						ExternalGhostManager.ExportPlayerDatabase();
+
+				if (GUILayout.Button(TempContent("Reset Ghost Cache", "This shouldn't be necessary unless something goes wrong"), CustomStyles.GhostButtonLayout))
+					ExternalGhostManager.ResetCache().LogFailure();
+			}
 
 			Styles.Space();
 
-			if (GUILayout.Button("Close"))
-				Close();
+			using (Styles.Horizontal())
+			{
+				GUILayout.FlexibleSpace();
+				if (GUILayout.Button("Close", Styles.FixedButton))
+					Close();
+			}
 		}
 
 		private void OpenGhostFolder()
@@ -75,6 +81,11 @@ namespace InertialOuija.UI
 				Directory.CreateDirectory(ExternalGhostManager.GhostsPath);
 
 			Process.Start(ExternalGhostManager.GhostsPath);
+		}
+
+		private static class CustomStyles
+		{
+			public static GUILayoutOption[] GhostButtonLayout = [GUILayout.Width(200)];
 		}
 	}
 }
