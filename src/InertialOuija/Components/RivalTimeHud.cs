9f2d96d4996bddbc196ad0b7f9f5bf9acf47eb7a
kalimag <kalimag@users.noreply.github.com>
2025-05-27 17:47:02 +0200

    Fix formatting

--- src/InertialOuija/Components/RivalTimeHud.cs
+++ src/InertialOuija/Components/RivalTimeHud.cs
@@ -6,55 +6,55 @@ namespace InertialOuija.Components;
 
 internal class RivalTimeHud : MonoBehaviour
 {
-    public static RivalTimeHud PersonalBestHud { get; set; }
-    public static RivalTimeHud FastestGhostHud { get; set; }
+	public static RivalTimeHud PersonalBestHud { get; set; }
+	public static RivalTimeHud FastestGhostHud { get; set; }
 
-    private TMP_Text _header;
-    private TMP_Text _name;
-    private TMP_Text _value;
+	private TMP_Text _header;
+	private TMP_Text _name;
+	private TMP_Text _value;
 
-    public string Header { get; private set; }
-    public string Name { get; private set; }
-    public string Value { get; private set; }
+	public string Header { get; private set; }
+	public string Name { get; private set; }
+	public string Value { get; private set; }
 
-    void Awake()
-    {
-        _header = transform.Find("HUDTitle Rival Time/Title").GetComponent<TMP_Text>();
-        _name = transform.Find("Rival Name/Rival Name").GetComponent<TMP_Text>();
-        _value = transform.Find("GoldTarget/Target Text").GetComponent<TMP_Text>();
-        UpdateText();
-    }
+	void Awake()
+	{
+		_header = transform.Find("HUDTitle Rival Time/Title").GetComponent<TMP_Text>();
+		_name = transform.Find("Rival Name/Rival Name").GetComponent<TMP_Text>();
+		_value = transform.Find("GoldTarget/Target Text").GetComponent<TMP_Text>();
+		UpdateText();
+	}
 
-    void OnDestroy()
-    {
-        if (PersonalBestHud == this)
-            PersonalBestHud = null;
-        if (FastestGhostHud == this)
-            FastestGhostHud = null;
-    }
+	void OnDestroy()
+	{
+		if (PersonalBestHud == this)
+			PersonalBestHud = null;
+		if (FastestGhostHud == this)
+			FastestGhostHud = null;
+	}
 
-    public void SetActive(bool value) => gameObject.SetActive(value);
+	public void SetActive(bool value) => gameObject.SetActive(value);
 
-    public void SetText(string header, string name, string value)
-    {
-        Header = header;
-        Name = name;
-        Value = value;
-    }
+	public void SetText(string header, string name, string value)
+	{
+		Header = header;
+		Name = name;
+		Value = value;
+	}
 
-    public void SetText(string header, string name, GhostTime value)
-    {
-        var valueStr = "<mspace=0.7em>" + value.ToString(false, ":");
-        SetText(header, name, valueStr);
-    }
+	public void SetText(string header, string name, GhostTime value)
+	{
+		var valueStr = "<mspace=0.7em>" + value.ToString(false, ":");
+		SetText(header, name, valueStr);
+	}
 
-    private void UpdateText()
-    {
-        if (_header)
-            _header.text = Header;
-        if (_name)
-            _name.text = Name;
-        if (_value)
-            _value.text = Value;
-    }
+	private void UpdateText()
+	{
+		if (_header)
+			_header.text = Header;
+		if (_name)
+			_name.text = Name;
+		if (_value)
+			_value.text = Value;
+	}
 }
