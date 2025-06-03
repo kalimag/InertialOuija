776ed5bd28299df2796618150340cc2ad41a483e
kalimag <kalimag@users.noreply.github.com>
2025-06-03 07:40:06 +0200

    Make version label more visible

--- src/InertialOuija/UI/Components/VersionLabel.cs
+++ src/InertialOuija/UI/Components/VersionLabel.cs
@@ -1,18 +1,22 @@
 ﻿extern alias GameScripts;
 using System.Reflection;
 using Doorstop;
+using GameScripts.Assets.Source.Messaging;
+using GameScripts.Assets.Source.Messaging.Messages;
+using GameScripts.Assets.Source.Tools;
 using GameScripts.Assets.Source.UI.Menus;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace InertialOuija.UI;
 
-internal class VersionLabel : MonoBehaviour
+internal class VersionLabel : BaseComponent, IReceiveMessages<ShowMainMenuMessage>, IReceiveMessages<HideMainMenuMessage>
 {
 	private string _label;
 
-	void Awake()
+	public override void Awake()
 	{
+		base.Awake();
+
 		useGUILayout = false;
 
 		var version = typeof(Entrypoint).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
@@ -21,18 +25,15 @@ internal class VersionLabel : MonoBehaviour
 		var build = typeof(Entrypoint).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration;
 		if (build != "Release")
 			_label += $" {build}";
-
-		SceneManager.activeSceneChanged += OnAciveSceneChanged;
 	}
 
-	private void OnAciveSceneChanged(Scene prev, Scene next)
-	{
-		enabled = next.name == "MainMenu";
-	}
+	void IReceiveMessages<ShowMainMenuMessage>.HandleMessage(ShowMainMenuMessage message) => enabled = true;
+	void IReceiveMessages<HideMainMenuMessage>.HandleMessage(HideMainMenuMessage message) => enabled = false;
 
 	void OnGUI()
 	{
-		if (!MenuNavigator.ActiveMenu || MenuNavigator.ActiveMenu.name != "MainMenuCanvas")
+		if (!GameData.CorePluginInitialized || CorePlugin.LoadingScreen.IsLoading || !MenuNavigator.ActiveMenu ||
+			MenuNavigator.ActiveMenu.name is not ("MainMenuCanvas" or "TrackSelectCanvas" or "EventSelectFreeDriveCanvas" or "CarSelectCanvas1"))
 			return;
 
 		Styles.ApplySkin();
